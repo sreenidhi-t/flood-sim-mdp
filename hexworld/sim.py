3063462762c961f852609a3f4bd5a6738ff7fcb5
BBalsara <43838069+BBalsara@users.noreply.github.com>
2023-03-01 22:38:35 -0800

    Update sim.py

--- hexworld/sim.py
+++ hexworld/sim.py
@@ -1,2 +1,13 @@
 from grid import Grid
 
+
+
+
+def main():
+    # Create grid
+    grid = Grid(10, 10)
+    # Draw grid
+    draw(grid, 'test.png', lambda h: (100, 0, 20), draw_edges=True)
+
+if __name__ == '__main__':
+    main()
