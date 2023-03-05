5e673fcac477d46cbea3e13ac6614190aa5cf63f
Mihir Kamble <mihir77.kamble@gmail.com>
2023-03-05 02:07:17 -0800

    Checking github status

--- hexworld/constants.py
+++ hexworld/constants.py
@@ -16,7 +16,7 @@ HEX_RECT_WIDTH = 2 * HEX_RADIUS
 FLOOD_LEVEL = 5
 SIM_TIME = 48
 PRECIP_RATE = 0.5 # 6 in/hr = max recorded rainfall rate from Claudette
-MAX_DRAIN_RATE = 1.5
+MAX_DRAIN_RATE = 1.5 # 
 FLOW_PER_LEVEL = 0.05
 PROB_DRAIN_FAIL = 0.0513/SIM_TIME # calculated lambda from exponential CDF of 0.05
 MAX_ELEV = 250  # average max elevation of cities along the Gulf Coast
