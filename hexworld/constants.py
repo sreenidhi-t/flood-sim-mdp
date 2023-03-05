ec9f21400db66346904be912620750b51b3f3c5f
Mihir Kamble <mihir77.kamble@gmail.com>
2023-03-05 15:36:27 -0800

    Tested working drain fail level

--- hexworld/constants.py
+++ hexworld/constants.py
@@ -18,9 +18,9 @@ SIM_TIME = 48
 PRECIP_RATE = 0.5 # 6 in/hr = max recorded rainfall rate from Claudette
 MAX_DRAIN_RATE = 1.5 # 
 FLOW_PER_LEVEL = 0.05
-PROB_DRAIN_FAIL = 0.0513/SIM_TIME # calculated lambda from exponential CDF of 0.05
+PROB_DRAIN_FAIL = 0.0513/SIM_TIME # calculated lambda from exponential CDF of 0.05 #TODO: Update for new grid size
 MAX_ELEV = 250  # average max elevation of cities along the Gulf Coast
 MIN_ELEV = 0  # minimum elevation of any cities along the Gulf Coast
 MAX_ELEV_DEV = 5
-DRAIN_FAIL_LEVEL = FLOOD_LEVEL/2 # level after which drain failure can occur
+DRAIN_FAIL_LEVEL = FLOOD_LEVEL/4 # level after which drain failure can occur
 
