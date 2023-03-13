00374fcb90fe11c13aa8758f02c360f1a84e97f6
Mihir Kamble <mihir77.kamble@gmail.com>
2023-03-12 23:25:54 -0700

    Implemented random rollout policy baseline

--- hexworld/mcts.py
+++ hexworld/mcts.py
@@ -1,6 +1,6 @@
 from world import World
 from water_sim import simRain, simDrain, simFlow, randDrainFail
-from constants import FLOOD_LEVEL, MAX_EVAC_CELLS, PRECIP_RATE, R_DRY_EVAC, R_DRY_NO_EVAC, R_FLOOD_EVAC, R_FLOOD_NO_EVAC
+from constants import FLOOD_LEVEL, MAX_EVAC_CELLS, PRECIP_RATE, R_DRY_EVAC, R_DRY_NO_EVAC, R_FLOOD_EVAC, R_FLOOD_NO_EVAC, SIM_TIME
 import numpy as np
 import itertools
 import random
@@ -15,6 +15,7 @@ class Node:
         self.children = []
         self.visits = 0
         self.reward = 0
+        self.curr_depth = 0
     
     def add_child(self, child):
         self.children.append(child)
@@ -31,7 +32,7 @@ class MCTS:
     def __init__(self, root: World):
         self.root = None  # State to perform MTCS on
         self.depth = 0  # Depth of tree
-        self.m = 0  # Number of simulations to run
+        self.m = 20  # Number of simulations to run
         self.c = 0  # Exploration constant
         self.tree = None  # Tree of nodes
 
@@ -48,7 +49,6 @@ class MCTS:
         next_state = randDrainFail(next_state)
         # one time step worth of drainage
         next_state = simDrain(next_state)
-
         return next_state
 
     def narrow_action_space(self, state: World):
@@ -60,10 +60,8 @@ class MCTS:
         coord_space = [(h.x, h.y) for h in hex_space]
         return coord_space
     
-    def calculate_reward(self, state: World, action):
+    def calculate_reward(self, state: World, action, next_state: World):
         reward = 0
-        next_state = self.get_next_state(state, action)
-        print(action)
         for hex in state.hexes:
             x, y = hex.x, hex.y
             # evacuated and flooded
@@ -93,48 +91,38 @@ class MCTS:
                 evac_hexes = action_space
             else:
                 evac_hexes = random.choices(action_space, k=MAX_EVAC_CELLS)
-            # calculate reward for this action step and add to the total utility
-            utility += self.calculate_reward(rollout_state, evac_hexes)
             # get the next state and repeat
-            rollout_state = self.get_next_state(rollout_state, evac_hexes)
+            next_state = self.get_next_state(rollout_state, evac_hexes)
+            # calculate reward for this action step and add to the total utility            
+            utility += self.calculate_reward(rollout_state, evac_hexes, next_state)
+            # get the next state and repeat
+            rollout_state = next_state
         return utility
 
-    def hallucinate(self, parent):
+    def get_branched_actions(self, state):
         # Get action space
-        coord_space = self.narrow_action_space(parent.state)
+        coord_space = self.narrow_action_space(state)
         # Create combinations of all actions
         action_space = []
-        
         if len(coord_space) < MAX_EVAC_CELLS:
             for i in range(0, len(coord_space) + 1):
-                action_space.append(itertools.combinations(coord_space), i)
+                for x in itertools.combinations(coord_space, i):
+                    action_space.append(list(x))
         else:
             for i in range(0, MAX_EVAC_CELLS + 1):
-                action_space.append(itertools.combinations(coord_space), i)
+                for x in itertools.combinations(coord_space, i):
+                    action_space.append(list(x))
 
         # Randomly choose m actions from action space
         if len(action_space) < self.m:
             actions = action_space
         else:
             actions = random.choices(action_space, k=self.m)
-               
-        
-        # For each action, get next state, then random rollout
-        for action in actions:
-            # Get next state
-            next_state = self.get_next_state(parent.state, action)
-            # Calculate reward
-            reward = self.random_rollout(next_state, 10)
-            # Create new node
-            new_node = Node(next_state)
-            new_node.action = action
-            new_node.parent = parent
-            new_node.update(reward)
-            parent.add_child(new_node)
-            # Update tree
-            self.tree.append(new_node)
 
-        # caluclate UCB1 heuristic for each node in parent's children
+        return actions
+
+    def get_best_action(self, parent):
+        # calculate UCB1 heuristic for each node in parent's children
         ucb1_values = [self.ucb1(child) for child in parent.children]
         # determine best action from index of maximum UCB1 value
         best_action = parent.children[ucb1_values.index(max(ucb1_values))].action
@@ -146,9 +134,50 @@ class MCTS:
         if node.visits == 0:
             return float("inf")
         # otherwise, return the UCB1 heuristic for the node
-        return (node.reward) + self.c * np.sqrt(np.log(node.parent.visits) / node.visits)
+        return (node.reward/node.visits) + self.c * np.sqrt(np.log(node.parent.visits) / node.visits)
     
+    def expand(self, node):
+        # Get action space
+        actions = self.get_branched_actions(node)
+        # Create child nodes for each action
+        for action in actions:
+            child = Node(self.get_next_state(node.state, action))
+            child.action = action
+            # child.curr_depth = node.curr_depth + 1
+            node.add_child(child)
     
+    def update_node_state(self, node):
+        # update the current state of the node
+        pass
+
+
+def RandAct(state: World, t):
+    obj = MCTS(state)
+    # get potential actions from given state
+    action_space = obj.get_branched_actions(state)
+    # generate potential next states from each action
+    if not action_space:
+        return action_space
+    else:
+        next_states = [obj.get_next_state(state, action) for action in action_space]
+    # generate random rollouts for each action
+    rollout_results = [obj.random_rollout(state, t) for state in next_states]
+    # determine best action index from rollout results
+    best_action = action_space[rollout_results.index(max(rollout_results))]
+    return best_action
+
+def RandPolicy(state: World):
+    t = 0
+    obj = MCTS(state)
+    net_reward = 0
+    while t < SIM_TIME:
+        print(t)
+        action = RandAct(state, SIM_TIME - t)
+        next_state = obj.get_next_state(state, action)
+        net_reward += obj.calculate_reward(state, action, next_state)
+        state = next_state
+        t += 1
+    return net_reward
 
 
 def main():
@@ -157,8 +186,8 @@ def main():
     # Create a MCTS object
     mcts = MCTS(world)
     # Get action space
-    a = mcts.random_rollout(world, 10)
-    print(a)
+    reward = RandPolicy(world)
+    print(reward)
 
 if __name__ == "__main__":
     main()
--- hexworld/world.py
+++ hexworld/world.py
@@ -130,10 +130,10 @@ class World:
     # tupleCoords is a list of coordinate tuples 
     def evacWorld(self, tupleCoords):
         numEvac = 0
-        # if tupleCoords is not empty
         if tupleCoords:
             for coord in tupleCoords:
-                pop2evac = self.grid[coord[0]][coord[1]].population
-                self.grid[coord[0]][coord[1]].evac(pop2evac)
-                numEvac += pop2evac
+                if coord:
+                    pop2evac = self.grid[coord[0]][coord[1]].population
+                    self.grid[coord[0]][coord[1]].evac(pop2evac)
+                    numEvac += pop2evac
         # return numEvac
